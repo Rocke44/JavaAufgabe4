56057d6a87b0bbc86626a845926d9e0171151e32
Jakob Rockenbauch <j.rockenbauch@tarent.de>
2016-11-02 14:47:58 +0100

    Basic Abstract Class Fortbewegungsmittel created

--- src/aufgabe4/Fortbewegungsmittel.java
+++ src/aufgabe4/Fortbewegungsmittel.java
@@ -11,7 +11,6 @@ public abstract class Fortbewegungsmittel {
 
     public Fortbewegungsmittel(double maxSpeed) {
         this.maxSpeed = maxSpeed;
-        this.speed = 0;
     }
 
     public double getPosition(){
@@ -27,16 +26,15 @@ public abstract class Fortbewegungsmittel {
             throw new FahrException("The speed most be positive");
         }
         if (speed > this.maxSpeed) {
-            throw new FahrException("The max. Speed for this Vehicle is: " + this.maxSpeed + "km/h!");
+            throw new FahrException("The max. Speed for this Vehicle is: " + this.maxSpeed + "km/h");
         }
         this.speed = speed;
     }
 
     public void move(double time){
         if(time<=0){
-            throw new FahrException("The time must be positive!");
+            throw new FahrException("The time must be positive");
         }
-        if(this.speed==0){throw new FahrException("You have to set the speed first!");}
         this.position += this.speed * (time / 60.0 );
     }
 
