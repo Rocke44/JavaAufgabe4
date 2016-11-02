55d387508e2645f59af6b7409c7f36ec90e4cd6b
Jakob Rockenbauch <j.rockenbauch@tarent.de>
2016-11-02 16:13:40 +0100

    b) fertiggestellt

--- src/aufgabe4/Krankenwagen.java
+++ src/aufgabe4/Krankenwagen.java
@@ -4,7 +4,27 @@ package aufgabe4;
  * Created by jrocke on 02.11.16.
  */
 public class Krankenwagen extends Fortbewegungsmittel {
+    private boolean blaulicht;
+
     public Krankenwagen() {
         super(160);
+        this.blaulicht=false;
     }
+
+    public void blaulichtAnschalten(){
+        this.blaulicht = true;
+    }
+    public void blaulichtAusschalten(){
+        this.blaulicht = false;
+    }
+
+    @Override
+    public void move(double time){
+        if(this.speed>50){
+            this.blaulichtAnschalten();
+            System.out.println("tatütata");
+        }
+        super.move(time);
+    }
+
 }
